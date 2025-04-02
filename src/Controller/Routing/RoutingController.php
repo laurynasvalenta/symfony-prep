293a595285d9911f39811b6d008c07f95a17d71d
Laurynas Valenta <64677926+laurynasvalenta@users.noreply.github.com>
2025-04-02 15:05:34 +0300

    Add implementations for Routing tests.

--- src/Controller/Routing/RoutingController.php
+++ src/Controller/Routing/RoutingController.php
@@ -8,10 +8,15 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[AsController]
 class RoutingController
 {
+    public function __construct(private readonly UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
     public function route1(): Response
     {
         return new Response('route1');
@@ -29,38 +34,38 @@ class RoutingController
         return new Response('With param');
     }
 
-    #[Route("/topic4/priority/static")]
+    #[Route("/topic4/priority/static", priority: 1)]
     public function higherPriority(): Response
     {
         return new Response('With static text');
     }
 
-    #[Route("/topic4/with-param1/{param}")]
+    #[Route("/topic4/with-param1/{param<\d+>}")]
     public function routeWithParam(string $param): Response
     {
         return new Response('Inlined param requirements. Param: ' . $param);
     }
 
-    #[Route("/topic4/with-param2/{param}")]
+    #[Route("/topic4/with-param2/{param}", requirements: ['param' => '\d+'])]
     public function routeWithParam2(string $param): Response
     {
         return new Response('Route requirements configured via a dedicated property. Param: ' . $param);
     }
 
-    #[Route("/topic4/default-param1/{param}")]
+    #[Route("/topic4/default-param1/{param?default1}")]
     public function defaultParam1(string $param): Response
     {
         return new Response('Param: ' . $param);
     }
 
-    #[Route("/topic4/default-param2/{param}")]
+    #[Route("/topic4/default-param2/{param}", defaults: ['param' => 'default2'])]
     public function defaultParam2(string $param): Response
     {
         return new Response('Param: ' . $param);
     }
 
     #[Route("/topic4/default-param3/{param}")]
-    public function defaultParam3(string $param): Response
+    public function defaultParam3(string $param = 'default3'): Response
     {
         return new Response('Param: ' . $param);
     }
@@ -70,17 +75,17 @@ class RoutingController
     public function generatedLink(): Response
     {
         return new Response(
-            sprintf('<a href="#">Link</a>')
+            sprintf('<a href="%s">Link</a>', $this->urlGenerator->generate('topic4-link-target'))
         );
     }
 
-    #[Route("/topic4/internal-route-attributes")]
+    #[Route("/topic4/internal-route-attributes/{_locale}/{_format}")]
     public function internalRouteAttributes(Request $request): Response
     {
         return new Response('Locale: ' . $request->getLocale());
     }
 
-    #[Route("/topic4/match-by-host")]
+    #[Route("/topic4/match-by-host", host: '%domain_to_match%')]
     public function matchByHost(): Response
     {
         return new Response('Matched by host');
--- src/Service/Loader/RoutesLoader.php
+++ src/Service/Loader/RoutesLoader.php
@@ -5,12 +5,24 @@ declare(strict_types=1);
 namespace App\Service\Loader;
 
 use Symfony\Component\Config\Loader\Loader;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
 class RoutesLoader extends Loader
 {
     public function load(mixed $resource, ?string $type = null): mixed
     {
-        return null;
+        $routes = new RouteCollection();
+
+        $path = '/topic4/custom-loaded-route';
+        $defaults = [
+            '_controller' => 'App\Controller\Routing\RoutingController::customLoadedRoute',
+        ];
+        $route = new Route($path, $defaults);
+
+        $routes->add('customLoadedRoute', $route);
+
+        return $routes;
     }
 
     public function supports(mixed $resource, ?string $type = null): bool
