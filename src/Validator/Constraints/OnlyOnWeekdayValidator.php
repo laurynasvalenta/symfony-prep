da706c79739be09adbb08ae4febded1ff80e7968
Laurynas Valenta <64677926+laurynasvalenta@users.noreply.github.com>
2025-04-23 17:16:51 +0300

    Add implementation for OnlyOnWeekdayConstraintTest.

--- src/Validator/Constraints/OnlyOnWeekdayValidator.php
+++ src/Validator/Constraints/OnlyOnWeekdayValidator.php
@@ -4,12 +4,38 @@ declare(strict_types=1);
 
 namespace App\Validator\Constraints;
 
+use Psr\Clock\ClockInterface;
 use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\Charset;
 use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 
 class OnlyOnWeekdayValidator extends ConstraintValidator
 {
+    public function __construct(
+        private ClockInterface $clock
+    ) {
+    }
+
     public function validate(mixed $value, Constraint $constraint): void
     {
+        if (!$constraint instanceof OnlyOnWeekday) {
+            throw new UnexpectedTypeException($constraint, Charset::class);
+        }
+
+        if (empty($value)) {
+            return;
+        }
+
+        $currentWeekday = $this->clock->now()->format('l');
+
+        if (in_array($currentWeekday, $constraint->getAllowedWeekdays())) {
+            return;
+        }
+
+        $this->context->buildViolation($constraint->message)
+            ->setParameter('{{ weekday }}', $currentWeekday)
+            ->setTranslationDomain('validation_errors')
+            ->addViolation();
     }
 }
