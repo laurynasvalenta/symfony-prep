b4cfe54a5e0ef74e402b65df17d95b16d1f012b0
Laurynas Valenta <64677926+laurynasvalenta@users.noreply.github.com>
2025-04-24 12:24:32 +0300

    Add implementations for Topic7Test.

--- src/Model/Order.php
+++ src/Model/Order.php
@@ -9,8 +9,18 @@ use Symfony\Component\Validator\Constraints\LessThan;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\GroupSequenceProviderInterface;
 
-class Order
+#[GroupSequenceProvider]
+class Order implements GroupSequenceProviderInterface
 {
+    #[NotBlank(message: 'You must provide product name.', groups: ['LightValidation'])]
     public ?string $productName = null;
+
+    #[NotBlank(message: 'You must provide order quantity.', groups: ['LightValidation'])]
+    #[LessThan(value: 1000, message: 'Your order exceeds the stock.', groups: ['HeavyValidation'])]
     public ?int $quantity = null;
+
+    public function getGroupSequence(): array
+    {
+        return ['LightValidation', 'HeavyValidation'];
+    }
 }
